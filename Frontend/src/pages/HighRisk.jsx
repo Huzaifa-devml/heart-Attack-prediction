9700695bf2597b26fd33dd53c0cb3e0370dcd894
Huzaifa Imam <your_email@gmail.com>
2026-01-29 22:23:27 +0530

    Fix CSS import case sensitivity for Vercel

--- Frontend/src/pages/HighRisk.jsx
+++ Frontend/src/pages/HighRisk.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import "./styles/Riskpages.css"
+import "./styles/RiskPages.css"
 
 function HighRisk() {
     const navigate=useNavigate()
--- Frontend/src/pages/LowRisk.jsx
+++ Frontend/src/pages/LowRisk.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import "./styles/Riskpages.css"
+import "./styles/RiskPages.css"
 
 function LowRisk() {
     const navigate=useNavigate()
